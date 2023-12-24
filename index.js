52f0cb158d4d2af2d8075acb734a17841c43ee17
kevin.silva <kevin.t.silva@protonmail.com>
2023-12-24 18:59:50 -0300

    feature: Site for get dogs random images;

---
+++ index.js
@@ -0,0 +1,23 @@
+const DOG_URL = "https://dog.ceo/api/breeds/image/random"
+
+const doggos = document.getElementById("dog-target")
+
+function addNewDoggo(){
+    const promise = fetch(DOG_URL)
+
+    promise
+    .then(function(response){
+        const processingPromise = response.text();
+        return processingPromise;
+    })
+    .then(function(processedReponse){
+        const dogObject = JSON.parse(processedReponse);
+        const img = document.createElement("img");
+
+        img.src = dogObject.message;
+        img.alt = "Cute Doggo";
+        doggos.appendChild(img)
+    });
+}
+
+document.getElementById("dog_button").addEventListener("click", addNewDoggo);
